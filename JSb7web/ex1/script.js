7e692085eeab53e45decca03f3e0eaf6d9e17f92
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-04-20 15:54:14 -0300

    exercicios

---
+++ JSb7web/ex1/script.js
@@ -0,0 +1,9 @@
+function alterar(titulo){
+
+    
+    document.getElementById('titulo').innerHTML = titulo;
+    document.getElementById('campo').value = titulo;
+
+}
+
+alterar("titulo de exemplo");
