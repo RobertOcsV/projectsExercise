45e7550b3494bd08f4aa784a008f968c5e6ee050
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-05-10 11:07:08 -0300

    Finalização do exercicio de while em JS

---
+++ JSb7web/ex11While/script.js
@@ -0,0 +1,42 @@
+//Dois tipos de for:
+
+// FOR LOOP ARRAY
+
+
+
+let html = '';
+
+let c = 0;
+while(c < 10){
+
+    html += "Número: "+c+"<br/>";
+
+    c++
+
+}
+
+
+document.getElementById('demo').innerHTML = html;
+
+
+
+
+
+
+ 
+
+// FOR LOOP
+/*
+let texto = '';
+
+for(let i = 0; i < 50; i++){
+
+    texto = texto + i + '<br/>';
+
+}
+
+
+*/
+
+
+
