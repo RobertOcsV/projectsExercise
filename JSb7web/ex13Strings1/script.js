18dd3dd6b94849d2f39a625ab85f7d4acece526b
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-05-12 10:36:19 -0300

    finalização do exercicio

---
+++ JSb7web/ex13Strings1/script.js
@@ -0,0 +1,39 @@
+
+let nome = 'este texto possui o nome: robert';
+let resultado = ' ';
+  if(nome.indexOf('rober') > -1){
+    resultado = "encontrou texto desejado";
+  }else{
+    resultado = "Não encontrou texto desejado";
+  }
+
+  console.log(resultado)
+
+
+
+
+
+
+// verifica se contém um texto, caracter em uma variável:
+
+// let nome = 'Bonieky Lacerda Leal';
+
+// let resultado = nome.indexOf('silva')
+
+// console.log(resultado)
+
+
+
+/*
+
+quantos caracteres há nessa var resultado ?:
+
+
+let nome = 'Bonieky Lacerda Leal';
+
+let resultado = nome.length
+
+console.log(resultado)
+
+
+*/
