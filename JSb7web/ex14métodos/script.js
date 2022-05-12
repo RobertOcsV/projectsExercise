4f0b7a9313999c23b90bc5d4515fa578773f8d56
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-05-12 11:05:47 -0300

    método slice e subtring

---
+++ JSb7web/ex14métodos/script.js
@@ -0,0 +1,26 @@
+
+let nome = 'este texto possui o nome: robert';
+
+let resultado = nome.slice(15, 20);
+
+console.log(resultado);
+
+
+
+
+/*
+
+let nome = 'este texto possui o nome: robert';
+
+let resultado = nome.slice(15, 20);
+
+console.log(resultado);
+
+  o método slice serve para puxar informações ou o texto desejado de uma variavel usando parametros de onde
+  começa e onde termina, exemplo acima.
+
+  não precisa colocar onde termina, mas é preciso colocar por onde começa
+
+  Se o numero colocado no parametro for negativo, ele irá começar a pegar do final do texto
+
+*/
