df48b632108f16ec99c0f14d5fa56c5006b282f9
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-06-03 17:49:42 -0300

    funções com o Math

---
+++ JSb7web/math/script.js
@@ -0,0 +1,58 @@
+
+let  novoValor = Math.round(3.67);
+
+console.log( novoValor);
+
+novoValor = Math.floor(3,99); // Podemos forçar o arredondamento de um numero com a classe floor
+
+console.log( novoValor);
+
+novoValor = Math.floor(3,99); //  Podemos forçar o arredondamento de um numero para cima  com a classe ceil
+
+console.log( novoValor);
+
+novoValor = Math.round(3.67); // arredonda pra cima ou pra baixo, depende se é mais próximo de 4 ou mais próximo de 3.
+
+console.log( novoValor);
+
+novoValor = Math.abs(-9.23434); // numero absoluto que transforma o numero em positivo.
+
+console.log( novoValor);
+
+novoValor = Math.random(); // vai retornar um numero aleatorio entre 0 e 1
+
+console.log( novoValor);
+
+
+//----
+
+ novoValor = Math.PI;
+
+console.log( novoValor);
+
+
+novoValor = Math.floor(Math.random() * 100); // vai retornar um numero aleatorio entre 0 e 1
+
+console.log( novoValor);
+
+
+
+
+
+/*
+É possível fazer cálculos com a classe Math
+
+A forma que mais iremos utilizar é o método de arredondamento de número.
+Existem três métodos de arredondamento, sendo eles:
+
+Podemos forçar o arredondamento de um numero para baixo com a classe floor
+
+Podemos forçar o arredondamento de um numero para cima  com a classe ceil
+
+Em, uma lista de numero dizer qual é o maior com o método: max
+
+Em, uma lista de numero dizer qual é o maior com o método = max
+
+função random retorna um valor entre 0 e 1, uma forma de poder faze-la se tornar mais útil é implementando em outras funções
+
+*/
