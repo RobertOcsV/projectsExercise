e10b0bb77929cbadc896cdd62acdcadee5df0140
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-06-08 15:34:54 -0300

    Finalização

---
+++ JSb7web/templateString/script.js
@@ -0,0 +1,11 @@
+let nome = 'Bonieky';
+let idade = 90;
+
+let frase = `Meu nome é ${nome}, eu tenho ${idade} anos e ano que vem eu farei ${idade+1} anos`;
+
+//let frase = 'Meu nome é '+ nome + ' e eu tenho ' + idade + ' anos' 
+
+
+console.log(frase)
+// Os templates strings são uma forma "nova" de criar strings.
+// o que é reconhecido pelo template string é o aspas `` para esquerda.
