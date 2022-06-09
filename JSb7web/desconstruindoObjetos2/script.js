413e72fd1916f88877352603ee13f7940d06f84a
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-06-09 09:35:29 -0300

    finalização

--- JSb7web/desconstruindoObjetos2/script.js
+++ JSb7web/desconstruindoObjetos2/script.js
@@ -1,43 +1,70 @@
 let pessoa = {
 
-    sobrenome:'Oliveira',
-    nome: 'Robert',
-    idade: undefined,
+    nome: 'Bonieky',
+    sobrenome: 'Lacerda',
+    idade:90,
     social:{
-        facebook:'b7web',
-        instagram:'Robert'
+        facebook:'Robert',
+        instagram:{
+            url: '@Robertin',
+            seguidores: 112
+        }
     },
     nomeCompleto: function() {
-        return `${this.nome} ${this.sobrenome}`;
+        return `${this.nome} ${this.idade}`;
     }
-}
+};
 
-console.log(pessoa.nomeCompleto())
+let {nome, idade, social:{instagram:rede}} = pessoa;
 
-// Processo de desconstrução//desustruturação de um objeto:
+console.log(nome, idade, rede);
 
-let {nome, sobrenome, idade = "não possui idade"} = pessoa;
+// como podemos consultar os atributos em que está dentro de um objeto que contém um objeto?
+//podemos usar a lógica dos ":" porém junto de um {}
+//exemplo:
+/*
 
-console.log(nome, sobrenome, idade)
+let { var1, var2, obj3:{var4}}
+porém como estamos selecionando um atributo de um objeto que está dentro de outro objeto, o unico que será 
+tornado em uma variável é o "var4" o obj3 se mantém em objeto e não vira variavel igual o var1 e var2.
 
-/*o código a cima é o mesmo que isso:
-    let nome = pessoa.nome;
-    let sobrenome = pessoa.sobrenome;
-    let idade = pessoa.idade;
-
-    --------
-    Ao desconstruir um objeto cada atributo antes que contiam dentro desse objeto se tornam variaveis externas
-    Que é possivel também alterar o nome delas ao ser desconstruido com o :
-    exemplo: let {nome:meunome,sobrenome}
-    agora a variavel que foi desconstruida terá o 'nome' da variavel de 'meunome'.
-
-    Podemos também definir um valor padrão para as variaveis desconstruidas. 
-    Caso a variavel não possua um dado, ela irá aparecer como undefined
-    e caso queiramos que possua um dado padrão para SOMENTE quando não houver um dado nele, podemos 
-    utilizar o "="
-    exemplo:
-    idade = ; 
-    let {nome, idade} = pessoa;
-
+agora iremos ver como usar functions com objetos:
 */
 
+ function pegarNomeCompleto (obj){
+     return `${obj.nome} ${obj.sobrenome}`
+ }
+
+ console.log(pegarNomeCompleto(pessoa))
+
+ // o que está acontecendo no código acima?
+
+//criamos uma function com um parametro de obj, dentro das chaves criamos o parametro.nome e parametro.sobrenome
+//ao utilizar o console.log pegamos a função e passamos o objeto pessoa para que faça objeto pessoa.nome e obj pessoa.sobrenome.
+
+//Outra forma de se fazer isso é:
+
+
+// function pegarNomeCompleto(obj){
+// let nome = obj.nome;
+// let sobrenome = obj.sobrenome;
+//
+// return `${nome} ${sobrenome}`;
+//
+// }
+//
+// console.log(pegarNomeCompleto(pessoa))
+
+/*
+    uma forma mais simplificada ainda de se fazer isso:
+*/
+
+function pegarNomeCompleto({nome, sobrenome = 'Ruperto', social:{instagram:{url:instagram}}}){
+    return `${nome} ${sobrenome} Siga em ${instagram}`
+}
+console.log(pegarNomeCompleto(pessoa))
+
+/* No código acima no momento em que estamos passando os parâmetros de nome, sobrenome ele já está
+sendo desconstruido, e dentro dele já podemos passar as funcionalidades anteriores como: ":" , "{}" ou "="
+
+*/
