7573f72c7ee6f0d072d3bbb0be9f2238074bf5e1
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-06-18 19:33:43 -0300

    callback definition

---
+++ JSb7web/Mod_req_assincr/Callbacks/script.js
@@ -0,0 +1,20 @@
+function alertar(){
+    alert('Opa, tudo bem?')
+
+}
+
+
+setTimeout(alertar, 2000);
+
+let nome = 'Bonieky';
+
+/*é uma função, assíncrona que quando uma condição, um tempo, um valor chegar 
+a determinada situação, executar algo
+fora da linha síncrona.
+*/
+
+/*
+
+Em outras palavras: Callback é a própria função que é utilizada como parametro de outra
+
+*/
