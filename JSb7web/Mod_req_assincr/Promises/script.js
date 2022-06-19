75deac5451eb42f556260017ebbff0a7e0c5edf4
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-06-19 04:36:06 -0300

    add comments

--- JSb7web/Mod_req_assincr/Promises/script.js
+++ JSb7web/Mod_req_assincr/Promises/script.js
@@ -38,3 +38,30 @@ console.log(pegarTemperatura)
     utilizamos o .catch()
  */
 //Usando a promise 
+
+/*
+ Um exemplo da utilização de promises seria em consulta de dados, podemos retornar json através do resolvem 
+ contendo o resultado desta busca.
+
+ As promises são uma das coisas mais utilizadas no desenvolvimento moderno. Só que geralmente vocÊ não 
+ vai crair uma promise, você vai receber ela de algum lugar.
+
+ Por exemplo quando você fizer uma consulta no banco de dados no Node, sua lib de ORM provavelmente vai 
+ te devolver uma promise.
+
+ Quando você faz uma requisição HTTP (para poder consumir uma API por exemplo), o fetch vai te retornar uma promise
+ Se você for usar o Node para ler um arquivo, você pode usar a versão com callbacks, mas também pode usar uma versão
+ que te devolve uma promise. Toda vez que você for utilizar algo que pode produzir um valor de forma assíncrona
+ muito provavelmente você vai acabar recebendo uma promise como retorno. A promise é o objeto que a gente usa para
+ representar um valor assíncrono(ou seja e le pode nem existir ainda quando ela for declarada no código, e você nem
+nem tem garantias 1ue ele vai existir em algum ponto no futuro), assim com,o quando a gente precisa representar
+uma lista de coisas, geralmente a gente usa um array, quando a gente precisa representar um valor assíncrono
+a gente usa uma promise.
+
+Muito raramente você vai precisar usar o construtor da promise já que a adoção pela comunidade foi muito grande
+então a maior parte das APIS da linguagem e das bibliotecas vão utilizas promises já, mas tem alguns casos onde
+ainda pode ser útil utilizar o construtor da Promise, por exemplo, para converter o setTimout da sua versão
+original em callback para uma versão em promise.
+
+
+*/
