db447f2f27e5ab6fb1b32dace7d4c334ed858c9e
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-06-27 11:06:47 -0300

    add images e ajust format folders

--- angular/projetoCursoAng/projetocursoang/src/app/app.module.ts
+++ angular/projetoCursoAng/projetocursoang/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
 import { HomeComponent } from './navegacao/home/home.component';
-import { FooterComponent } from './navegacao/footer/footer.component';
+import { FooterComponent } from './navegacao/home/footer/footer.component';
 
 @NgModule({
   declarations: [
