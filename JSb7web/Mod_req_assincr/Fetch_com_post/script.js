80ff1b326f614a1e3663673d6023a153ad5e41b4
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-06-29 14:31:55 -0300

    requisições fetch com post(revisar)

--- JSb7web/Mod_req_assincr/Fetch_com_post/script.js
+++ JSb7web/Mod_req_assincr/Fetch_com_post/script.js
@@ -1,8 +1,21 @@
 async function inserirPost() {
-    document.getElementById("post").innerHTML = ' Carregando...'
+    document.getElementById("posts").innerHTML = "Carregando...";
+   
 
     let req = await fetch('https://jsonplacecholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title:'Titulo de teste',
+            body: 'Corpo de teste',
+            userId: 4
+        }),
+        headers:{
+            'Content-Type': 'application/json'
         
+        }
     })
+    let json = await req.json();
 
+    console.log(json)
 }
+
