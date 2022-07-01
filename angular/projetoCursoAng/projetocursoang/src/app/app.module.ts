4d1a19e6fad1e918195e183d061b01235ab34638
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-07-01 13:57:08 -0300

    Adicionando modulos de rotas

--- angular/projetoCursoAng/projetocursoang/src/app/app.module.ts
+++ angular/projetoCursoAng/projetocursoang/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
@@ -10,6 +11,8 @@ import { HomeComponent } from './navegacao/home/home.component';
 import { FooterComponent } from './navegacao/home/footer/footer.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
+import { DataBindingComponent } from './demos/data-binding/data-binding.component';
+
 
 @NgModule({
   declarations: [
@@ -18,10 +21,12 @@ import { ContatoComponent } from './institucional/contato/contato.component';
     HomeComponent,
     FooterComponent,
     SobreComponent,
-    ContatoComponent
+    ContatoComponent,
+    DataBindingComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     [RouterModule.forRoot(rootRouterConfig, {useHash: false})]
   ],
   providers: [
