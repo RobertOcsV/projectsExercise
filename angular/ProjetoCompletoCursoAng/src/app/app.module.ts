60db1315fc52f78a15f949c864bdf2b330a8ec7a
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-07-05 17:15:32 -0300

    adicionando projeto completo angular professor

--- angular/ProjetoCompletoCursoAng/src/app/app.module.ts
+++ angular/ProjetoCompletoCursoAng/src/app/app.module.ts
@@ -12,13 +12,14 @@ registerLocaleData(localePt);
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
 import { HomeComponent } from './navegacao/home/home.component';
-import { FooterComponent } from './navegacao/home/footer/footer.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
+import { rootRouterConfig } from './app.routes';
 import { DataBindingComponent } from './demos/data-binding/data-binding.component';
 import { ProdutoService } from './produtos/produtos.service';
 import { ListaProdutoComponent } from './produtos/lista-produto/lista-produto.component';
-import { rootRouterConfig } from './app.routes';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,19 +30,16 @@ import { rootRouterConfig } from './app.routes';
     ContatoComponent,
     DataBindingComponent,
     ListaProdutoComponent
-    
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
     [RouterModule.forRoot(rootRouterConfig, { useHash: false})]
-    
   ],
   providers: [
-      ProdutoService,
-      {provide: APP_BASE_HREF, useValue: '/'}
-
+    ProdutoService,
+    {provide: APP_BASE_HREF, useValue: '/'}
   ],
   bootstrap: [AppComponent]
 })
