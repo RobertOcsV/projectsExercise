d3c3e97c23f19cd2920e51d6f28734bc032e3d8d
Robert <61297366+RobertOcsV@users.noreply.github.com>
2022-11-06 03:21:54 -0300

    expecificações padroes do projeto + ngbootstrap

--- angular/landingPageTest/src/app/app-routing.module.ts
+++ angular/landingPageTest/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CabecarioComponent } from './components/cabecario/cabecario.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{
+path: '',
+component: CabecarioComponent
+
+}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular/landingPageTest/src/app/app.module.ts
+++ angular/landingPageTest/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CabecarioComponent } from './components/cabecario/cabecario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecarioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
